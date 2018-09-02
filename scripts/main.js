2cd55e1d4caf6f459f714850c5a62f7920249eff
Chukwuemeka <chukwuemekandukwe01@gmail.com>
2018-09-02 21:36:58 +0100

    proper initial commit

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+
+function rollover(my_image)
+    {
+
+        my_image.src = 'images/profile-2.jpg';
+
+    }
+
+function mouseaway(my_image)
+    {
+
+        my_image.src = 'images/profile.jpg';
+
+    }
+
+function myFunction() {
+    document.getElementById("about").innerHTML = "I am a developer in training, i am very curious about how blockchain can be used to improve governance in African countries considering that transparency is one of our biggest issues ";
+}
+
